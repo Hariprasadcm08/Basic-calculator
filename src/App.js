371501ebaf071a205dea6c2893623036017de3d7
Hariprasadcm08 <hariprasadcm08@gmail.com>
2023-03-31 09:07:40 +0530

    completed basic implimentation

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Table from './Table'
+import Change from './Change'
+import Form from './form'
+import Buttn from './Buttn'
+
+function App() {
+  return (
+    <div>
+      <Table />
+      <Change />
+      <Form />
+
+      <Buttn />
+
+      
+    </div>
+  )
+}
+
+export default App
